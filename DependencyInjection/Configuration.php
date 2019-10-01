ab50e5d604c638e20767242d75ddda9c951e0a50
kravchuk.d <kravchuk.d@prodv.net>
2019-10-01 10:17:44 +0700

    sf4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,8 +18,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode    = $treeBuilder->root($this::ROOT);
+        $treeBuilder = new TreeBuilder($this::ROOT);
+        $rootNode    = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
